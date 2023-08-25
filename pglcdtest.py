c681296cf820501809c3579a6906b19a82e5dd4d
Mellor <jim.mellor@me.com>
2023-08-25 21:43:59 +0100

    print information about the display and  modes

--- pglcdtest.py
+++ pglcdtest.py
@@ -6,17 +6,28 @@ import pygame
 # Initialise pygame
 pygame.init()
 
+# print information about the display
+print(pygame.display.Info())
+
+# print informaion  about the display modes
+print(pygame.display.list_modes())
+
+
 # set the screen size
 screen = pygame.display.set_mode((400, 1280))
 
-#draw a rectangle filling the screen with yellow
-screen.fill((255, 255, 0))
+#draw a rectangle
+pygame.draw.rect(screen, (255, 255, 255), (0, 0, 400, 1280))
+
 
 #display text at 90 degrees clockwise, filling the full height of the display saying "THIS WAY UP"
 font = pygame.font.SysFont("Arial", 250)
 text = font.render("THIS WAY UP", True, (0, 0, 0))
 text = pygame.transform.rotate(text, 90)
 screen.blit(text, (0, 0))
+
+# rota the screen 90 degrees clockwise
+screen = pygame.transform.rotate(screen, 90)
 
 #update the display
 pygame.display.update()
