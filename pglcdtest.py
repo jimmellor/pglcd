8aed8840790787e1f78d91da1c858562846b03e6
Mellor <jim.mellor@me.com>
2023-08-25 23:21:14 +0100

    draw and show the cursor postiion

--- pglcdtest.py
+++ pglcdtest.py
@@ -16,69 +16,50 @@ print(pygame.display.list_modes())
 # set the screen size
 screen = pygame.display.set_mode((400, 1280))
 
-#draw a rectangle
-pygame.draw.rect(screen, (255, 255, 255), (0, 0, 400, 1280))
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == pygame.KEYDOWN:
+            # Was it the Escape key? If so, stop the loop
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop
+        elif event.type == pygame.QUIT:
+            running = False 
+    #draw a rectangle
+    #pygame.draw.rect(screen, (255, 255, 255), (0, 0, 1280, 400))
 
 
-#display text at 90 degrees clockwise, filling the full height of the display saying "THIS WAY UP"
-font = pygame.font.SysFont("Arial", 250)
-text = font.render("THIS WAY UP", True, (0, 0, 0))
-text = pygame.transform.rotate(text, 90)
-screen.blit(text, (0, 0))
+        #display the position of the mouse on the screen with a small circle
+        mousepos= pygame.mouse.get_pos()
+        pygame.draw.circle(screen, (255, 0, 0), (mousepos[0],mousepos[1]), 10)
 
-# rota the screen 90 degrees clockwise
-screen = pygame.transform.rotate(screen, 90)
 
-#update the display
-pygame.display.update()
+        #display text at 90 degrees clockwise, filling the full height of the display saying "THIS WAY UP"
+        font = pygame.font.SysFont("Arial", 12)
+        mousepos= pygame.mouse.get_pos()
+        text = font.render(f"{mousepos[0],mousepos[1]}", True, (255, 255, 255))
+        #text = pygame.transform.rotate(text, 90)
+        #draw a black rectangle to clear the screen
+        pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1280, 400))
 
-# wait for 5 seconds
-pygame.time.wait(2000)
+        screen.blit(text, (0, 0))
 
-# draw a rectangle filling the screen with blue
-screen.fill((0, 0, 255))
 
-#display text at 90 degrees clockwise, filling the full height of the display saying "THIS WAY UP"
-font = pygame.font.SysFont("Arial", 250)
-text = font.render("THIS WAY UP", True, (0, 0, 0))
-text = pygame.transform.rotate(text, 90)
-screen.blit(text, (0, 0))
+        # rotate the screen 90 degrees clockwise
 
-# update the display
-pygame.display.update()
+        #screen.blit(pygame.transform.rotate(screen, 90), (0, 650))
 
-# wait for 5 seconds
-pygame.time.wait(2000)  
+        #update the display
+        pygame.display.update()
 
-# draw a rectangle filling the screen with red
-screen.fill((255, 0, 0))
-
-#display text at 90 degrees clockwise, filling the full height of the display saying "THIS WAY UP"
-font = pygame.font.SysFont("Arial", 250)
-text = font.render("THIS WAY UP", True, (0, 0, 0))
-text = pygame.transform.rotate(text, 90)
-screen.blit(text, (0, 0))
-
-# update the display
-pygame.display.update()
-
-# wait for 5 seconds
-pygame.time.wait(2000)
-
-# draw a rectangle filling the screen with green
-
-screen.fill((0, 255, 0))
-#display text at 90 degrees clockwise, filling the full height of the display saying "THIS WAY UP"
-font = pygame.font.SysFont("Arial", 250)
-text = font.render("THIS WAY UP", True, (0, 0, 0))
-text = pygame.transform.rotate(text, 90)
-screen.blit(text, (0, 0))
-
-# update the display
-pygame.display.update()
-
-# wait for 5 seconds
-pygame.time.wait(2000)
+    # wait for 5 seconds
+    #pygame.time.wait(5000)
 
 # end the programme
 pygame.quit()
